cb13da2dc01135b479aad6644a3708a4fa07bd77
Pandava919 <pandavapandu919@gmail.com>
2025-01-18 23:44:20 +0530

    posted the data

--- frontend/src/Components/Admin/AddCandidates.jsx
+++ frontend/src/Components/Admin/AddCandidates.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import '../../Styles/AddCandidate.css'
 import UseFetch from '../../Hooks/UseFetch';
+import axios from 'axios';
 
 const AddCandidates = () => {
     const [formData, setFormData] = useState({
@@ -10,31 +11,80 @@ const AddCandidates = () => {
         candidate_address: "",
         candidate_photo: ""
     });
-    const [elections, setElections] = useState()
-
+    const [errors, setErrors] = useState({});
+    const [elections, setElections] = useState([]);
+    //! fetching the elections
     const { apiData, Loading } = UseFetch('http://localhost:4000/api/onlinevoting/get-elections');
     useEffect(() => {
         setElections(apiData?.data)
     }, [apiData])
+    //! filter the elections
     const electionTopics = elections?.filter((election) => election.status = "active")
-    const onFileChangeHandler = (e) => {
-        console.log(e.target.files);
+    //! image handler
+    const onImageFileHandler = (e) => {
+        const imageFile = e.target.files[0];
+        console.log(imageFile);
+
+        if (imageFile) {
+            const imageFileReader = new FileReader(); //filereader in react
+            imageFileReader.onload = () => {
+                setFormData({ ...formData, candidate_photo: imageFileReader.result }) // conver to base64 string
+            };
+
+            imageFileReader.readAsDataURL(imageFile)
+        } else {
+            alert('Select the image')
+        }
     }
 
+    //! input change handler
     const onChangeHandler = ({ target }) => {
         let { name, value } = target;
         setFormData({ ...formData, [name]: value })
+    }
+    //! imput validation
+    const candidateInputHandler = (formData) => {
+        let errors = {};
+        if (!formData?.candidate_name) {
+            errors.candidate_name = 'Candidate name is mandatory'
+        }
+        if (!formData?.election_topic) {
+            errors.election_topic = 'Election topic is mandatory'
+        }
+        if (!formData?.candidate_contact) {
+            errors.candidate_contact = 'Candidate contact is mandatory'
+        }
+        if (!formData?.candidate_address) {
+            errors.candidate_address = 'candidate address is mandatory'
+        }
+        if (!formData?.candidate_photo) {
+            errors.candidate_photo = 'candidate photo is mandatory'
+        }
+        return errors
+    }
+    //! onsubmit hander
+    const onSubmitHandler = async (e) => {
+        e.preventDefault()
+        try {
+            console.log(formData.candidate_photo);
+            await axios.post('http://localhost:4000/api/onlinevoting/add-candidate', formData)
+
+        } catch (error) {
+            console.log(error);
+            alert(error.message)
+        }
     }
     return (
         <section className='addcandidate-main-container'>
             <div className="addcandidate-form-container">
                 <h1>Add New Candidate</h1>
-                <form>
+                <form onSubmit={onSubmitHandler}>
                     <div className="input-container">
                         <select name="election_topic" value={formData.election_topic} onChange={onChangeHandler}>
-                            {elections?.map((election) => {
+                            <option value="" disabled>Select Election</option>
+                            {electionTopics?.map((election) => {
                                 return (
-                                    <option value="election.election_topic" > {election.election_topic}</option>
+                                    <option value={election?._id} key={election?._id} > {election.election_topic}</option>
                                 )
                             })}
                         </select>
@@ -49,17 +99,17 @@ const AddCandidates = () => {
                         {/* {errors?.no_of_candidates && <span>{errors.no_of_candidates}</span>} */}
                     </div>
                     <div className="input-container">
-                        <input type="number" min={10} max={10} name='candidate_contact' value={formData.candidate_contact} placeholder='Candidate Contact' onChange={onChangeHandler} />
+                        <input type="text" minLength={10} maxLength={10} name='candidate_contact' value={formData.candidate_contact} placeholder='Candidate Contact' onChange={onChangeHandler} />
                         {/* {errors?.no_of_candidates && <span>{errors.no_of_candidates}</span>} */}
                     </div>
 
                     <div className="input-container">
-                        <input type="file" name='' placeholder='Candidate Image' onChange={onFileChangeHandler} />
+                        <input type="file" placeholder='Candidate Image' onChange={onImageFileHandler} />
                         {/* {errors?.starting_date && <span>{errors.starting_date}</span>} */}
                     </div>
 
                     <div className="btn-container">
-                        <button type='submit'>Login</button>
+                        <button type='submit'>Add Candidate</button>
                     </div>
                 </form>
             </div>
