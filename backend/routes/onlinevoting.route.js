aaaaeb13b1f10969adac3d65f1dd58a5c6c2d665
Pandava919 <pandavapandu919@gmail.com>
2025-01-19 20:41:03 +0530

    added a route

--- backend/routes/onlinevoting.route.js
+++ backend/routes/onlinevoting.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const { addElection, addCandidate, UserRegistrion, userLogin, getElections, deleteElection } = require('../controllers/admin.controller')
+const { addElection, addCandidate, UserRegistrion, userLogin, getElections, getCandidates, deleteElection } = require('../controllers/admin.controller')
 let route = express.Router()        //router
 
 route.post('/register', UserRegistrion)
 route.post('/login', userLogin)
 route.post('/addelection', addElection)     //router to create election
+route.post('/addcandidate', addCandidate)       //router to add candidate
 route.get('/get-elections', getElections)
+route.get('/get-candidates', getCandidates)
 route.delete('/delete-election/:id', deleteElection)
-route.post('/add-candidate', addCandidate)       //router to add candidate
 
 module.exports = route 
